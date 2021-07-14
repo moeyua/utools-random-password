8380aa4e40e943ee355193f61ea143ba831d134c
moeyua <moeyua@foxmail.com>
2021-07-14 15:54:01 +0800

    fix:清除打包后的map调试文件

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:moeyua/random-password.git master:gh-pages
+git push -f git@github.com:Moeyua/random-password.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/random-password/'
-    : '/'
+    // publicPath: process.env.NODE_ENV === 'production'
+    // ? '/random-password/'
+    // : '/'
+    publicPath: './',
+    productionSourceMap: false,
 }
