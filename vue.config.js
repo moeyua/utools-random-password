96a923273462d019c40894d385647aaf81f0d00a
moeyua <moeyua@foxmail.com>
2021-07-19 17:04:08 +0800

    fix:publicPath路径修改

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    // publicPath: process.env.NODE_ENV === 'production'
-    // ? '/random-password/'
-    // : '/'
-    publicPath: './',
-    productionSourceMap: false,
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/random-password/'
+    : '/'
+    // publicPath: './',
+    // productionSourceMap: false,
 }
