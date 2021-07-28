6a862bdc459e05bc69e6844fad12037528990cd5
moeyua <moeyua@foxmail.com>
2021-07-28 18:12:03 +0800

    update:插件更新

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,25 @@
-// const CopyPlugin = require("copy-webpack-plugin");
-
+// utools打包时使用这个
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/random-password/'
-    : '/'
-    // publicPath: './',
-    // productionSourceMap: false,
-}
+    // publicPath: process.env.NODE_ENV === 'production'
+    // ? '/random-password/'
+    // : '/'
+    publicPath: './',
+    productionSourceMap: false,
+    configureWebpack: {
+        plugins: [
+            new CopyPlugin(
+                [
+                    { from: 'utools' }
+                ]
+            ),
+        ],
+    }
+} 
+
+// git-pages打包使用这个
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//     ? '/random-password/'
+//     : '/'
+// }
