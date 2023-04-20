1e04493680ea83657a407cb500d5d9b9990fb14c
moeyua <moeyua@foxmail.com>
2023-04-20 17:24:25 +0800

    🦄 refactor: rebuild with vue3 + vite + ts

--- README.md
+++ README.md
@@ -1,48 +1,46 @@
 # random-password
-能够简单的生成随机密码
-- 目前特殊符号支持 "~", "!", "@", "#", "$", "%", "^", "&", "*", "?", "_", "-"
 
-## 预览插件
-![预览](https://i.loli.net/2021/07/19/Kb2WuFRr8LwnGaH.png)
+This template should help get you started developing with Vue 3 in Vite.
 
+## Recommended IDE Setup
 
-## 插件使用
-1. 下载并安装[utools](https://u.tools/)
-2. 在插件中心搜索【随机密码】并下载，或者在[random-password](https://github.com/Moeyua/random-password/releases)下载安装包
-3. 将文件拖入到主窗口安装插件
-4. 启用插件
-5. 通过关键字["密码", "随机", "password", "random", "pwd"]快速启动
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
 
-## web使用（通过浏览器直接使用）
-https://moeyua.github.io/random-password/
+## Type Support for `.vue` Imports in TS
 
+TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin) to make the TypeScript language service aware of `.vue` types.
 
+If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has also implemented a [Take Over Mode](https://github.com/johnsoncodehk/volar/discussions/471#discussioncomment-1361669) that is more performant. You can enable it by the following steps:
 
-## utoos plugin
-- utools版本在utools-plugin分支
-- vue.config.js中的publicPath为`./`
-- ~~ 将utools文件夹中的文件在打包后复制一份到dist根目录，并在修改后及时备份 ~~ 
-- utools的配置文件会自动打包进dist目录
+1. Disable the built-in TypeScript Extension
+    1) Run `Extensions: Show Built-in Extensions` from VSCode's command palette
+    2) Find `TypeScript and JavaScript Language Features`, right click and select `Disable (Workspace)`
+2. Reload the VSCode window by running `Developer: Reload Window` from the command palette.
 
-## Project setup
-```
-npm install
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+pnpm install
 ```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
+### Compile and Hot-Reload for Development
+
+```sh
+pnpm dev
 ```
 
-### Compiles and minifies for production
-```
-npm run build
+### Type-Check, Compile and Minify for Production
+
+```sh
+pnpm build
 ```
 
-### Lints and fixes files
-```
-npm run lint
-```
+### Lint with [ESLint](https://eslint.org/)
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+```sh
+pnpm lint
+```
--- src/App.vue
+++ src/App.vue
@@ -1,144 +1,47 @@
-<template>
-  <div id="app">
-    <el-form label-width="100px">
-
-      <el-form-item label-width="0">
-        <el-input v-model="password" id="password" size="large" disabled>
-          <div slot="suffix" style="background-color:#FFF;height: 38px;margin-top:1px">
-          <el-button icon="el-icon-refresh" circle  class="refresh-button"
-          :class="{'rotate360':showAnimate}" @click="play"
-          ></el-button>
-          </div>
-          <el-button slot="append" @click="copyToClip(password)">复制</el-button>
-        </el-input>
-      </el-form-item>
-
-      <el-form-item label="密码长度：">
-        <el-slider v-model="length" @input="generate"></el-slider>
-      </el-form-item>
-
-      <el-form-item label="可用字符：">
-        <el-checkbox-group v-model="symbolType" @change="generate">
-          <el-checkbox label="upper">ABC</el-checkbox>
-          <el-checkbox label="lower">abc</el-checkbox>
-          <el-checkbox label="number">123</el-checkbox>
-          <el-checkbox label="symbol">#$*</el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-    </el-form>
-  </div>
-</template>
-
-<script>
-var Mock = require('mockjs')
-export default {
-  name: "App",
-  components: {
-  },
-  data() {
-    return {
-      password: "",
-      length: 15,
-      symbolType: ["lower","upper","number"],
-      lower: "abcdefghijklmnopqrstuvwxyz",
-      upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-      number: "0123456789",
-      symbol: "!@#$%^&*()[]",
-      showAnimate: false
-    };
-  },
-  mounted() {
-    this.generate();
-  },
-  methods: {
-    pool() {
-      let symbolType = this.symbolType;
-      let pool = "";
-      for (const i in symbolType) {
-        switch (symbolType[i]) {
-        case "upper":
-          pool = pool + this.upper;
-          break;
-        case "lower":
-          pool = pool + this.lower;
-          break;
-        case "number":
-          pool = pool + this.number;
-          break;
-        case "symbol":
-          pool = pool + this.symbol;
-          break;
-        }
-      }
-      return pool;
-    },
-    generate() {
-      this.password = Mock.Random.string( this.pool(), this.length );
-    },
-    copyToClip(content) {
-      let _this = this;
-      navigator.clipboard.writeText(content).then(function() {
-        _this.$notify({
-          title: '成功',
-          message: '复制成功',
-          type: 'success'
-        })
-      }, function () {
-        _this.$notify.error({
-          title: '失败',
-          message: '复制失败',
-        })
-      })
-    },
-    play() {
-      this.showAnimate = true;
-      this.generate();
-      setTimeout(()=>{
-        this.reset()
-      }, 500 )    
-    },
-    reset() {
-      this.showAnimate = false
-    }
-  },
-};
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  width: 60%;
-  margin-left: 20%;
-  margin-top: 20%;
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
 }
-#password {
-  margin-left: 10px;
-  
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
 }
-.el-input.is-disabled .el-input__inner {
-  background-color: #fff;
-  color: #000;
-}
-.el-input.is-disabled .el-input__inner {
-  cursor: text;
-}
-.el-input-group__append, .el-input-group__prepend {
-  background-color: rgb(64, 158, 255);
-  color: #fff;
-}
-.refresh-button {
-  font-size: 20px;
-  border: none;
-  background-color:rgba(255,255,255,0);
-}
-@keyframes rotate360 {
-  100% {
-    transform: rotate(360deg);
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
-}
-.rotate360 {
-    animation: rotate360 .5s ease-out 0s;
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
